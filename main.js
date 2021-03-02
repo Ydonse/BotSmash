d1e5c6aac1e80b321b422b7eb091362d13c48127
root <root@DESKTOP-QB1EKJP.localdomain>
2021-03-02 22:11:00 +0100

    fix url

--- main.js
+++ main.js
@@ -182,6 +182,7 @@ async function chooseStage(user1, user2, args, message)
   if (levelsRemaining === 1)
   {
     message.channel.send(`${user1} ${user2} Que le match commence !`, {files : [allImages[levels[0] - 1]]});
+    .catch((error) => {console.log(error)} );
   }
 }
 
