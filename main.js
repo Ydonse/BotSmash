140e4a3313851741cc6e083feac19cc996c100c9
Yohann Donse <yohann.donse@gmail.com>
2021-04-14 00:44:58 +0200

    fix log color

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const token = process.env.TOKEN;
 client.once("ready", async () => {
   console.log("Nb de serveurs utilisant le bot : " + client.guilds.cache.size);
   client.guilds.cache.forEach((guild) => {
-    console.log(guild.name, "color:green");
+    console.log(`%c ${guild.name}`, "color:green");
   });
   client.user.setActivity("!banstage", { type: "PLAYING" });
   await IMG.loadImages().catch((error) => {
