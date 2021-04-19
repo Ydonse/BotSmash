9ad01f3b3f26baba087fb0e0e2806c7ee973500d
Yohann Donse <yohann.donse@gmail.com>
2021-04-19 14:05:21 +0200

    test canvas sized on image width

--- stage/canvas.js
+++ stage/canvas.js
@@ -77,7 +77,7 @@ module.exports = class DrawCanvas {
 
   static getImageWidth(levels, canvasX) {
     let imagesNb = levels.length > 3 ? 4 : levels.length;
-    return canvasX / imagesNb - imagesNb * espacementX;
+    return canvasX / imagesNb - espacementX;
   }
 
   static getImageHeight(levels, canvasY) {
